1a07cb99ff20a07186b2ef116f69e717c0c23bcd
eric9530873 <eric9530873@gmail.com>
2023-04-16 23:33:28 +0800

    4/16

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -7,6 +7,25 @@ const adminController = {
         })
             .then(restaurants => res.render('admin/restaurants', { restaurants: restaurants }))
             .catch(err => next(err))
+    },
+    createRestaurant: (req, res, next) => {
+        res.render('admin/create-restaurant')
+    },
+    postRestaurant: (req, res, next) => {
+        if (!req.body.name) throw new Error('Restaurant name is required')
+
+        Restaurant.create({
+            name: req.body.name,
+            tel: req.body.tel,
+            address: req.body.address,
+            openingHours: req.body.openingHours,
+            description: req.body.description
+        })
+            .then(() => {
+                req.flash('success_msg', 'restaurant was successfully created')
+                res.redirect('/admin/restaurants')
+            })
+            .catch(err => next(err))
     }
 }
 
